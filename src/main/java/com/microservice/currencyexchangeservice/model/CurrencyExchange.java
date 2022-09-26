32179ec9c7f7a773cd187d03b502e5ed6154282a
Dhananjay Singh <dhananjaysinghkushwaha2@gmail.com>
2022-09-26 20:42:01 +0530

    139. Step 13 - Configure JPA and Initialized Data - V2

--- src/main/java/com/microservice/currencyexchangeservice/model/CurrencyExchange.java
+++ src/main/java/com/microservice/currencyexchangeservice/model/CurrencyExchange.java
@@ -4,15 +4,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 @Data
 @NoArgsConstructor
 @ToString
 public class CurrencyExchange {
 
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
     private BigDecimal conversionMultiple;
     private String environment;
