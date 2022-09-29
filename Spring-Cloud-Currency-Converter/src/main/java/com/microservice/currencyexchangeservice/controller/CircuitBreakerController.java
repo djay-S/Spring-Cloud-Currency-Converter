8fa7f8e81bc135676742ffc25d0cfb21c87d1e41
Dhananjay Singh <dhananjaysinghkushwaha2@gmail.com>
2022-09-29 20:32:55 +0530

    159. Step 27 - Playing with Resilience4j - Retry and Fallback Methods

--- Spring-Cloud-Currency-Converter/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ Spring-Cloud-Currency-Converter/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,32 @@
 package com.microservice.currencyexchangeservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.UndeclaredThrowableException;
 
 @RestController
 public class CircuitBreakerController {
 
+    private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
     @GetMapping("/sample-api")
+//    @Retry(name = "default") annotation retries the failing api call.
+//    If name is 'default' it retries for 3 times, custom name can be specified at the application.properties file
+//    fallbackMethod tells which method to be invoked when the request fails
+    @Retry(name = "sample-api", fallbackMethod = "fallbackMethod")
     public String sampleApi() {
-        return "Sample API";
+        logger.info("Sample Api Called.");
+        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8081/this-url-fails", String.class);
+        return forEntity.getBody();
+    }
+
+    public String fallbackMethod(Exception e) {
+        return "Fallback Method: \n" + e;
     }
 }
