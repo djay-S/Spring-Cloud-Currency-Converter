960cd6dc6b1d00281790c96ba61182c07c253bbc
Dhananjay Singh <dhananjaysinghkushwaha2@gmail.com>
2022-09-30 19:13:31 +0530

    160. Step 28 - Playing with Circuit Breaker Features of Resilience4j

--- Spring-Cloud-Currency-Converter/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
+++ Spring-Cloud-Currency-Converter/src/main/java/com/microservice/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,5 +1,6 @@
 package com.microservice.currencyexchangeservice.controller;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.lang.reflect.UndeclaredThrowableException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @RestController
 public class CircuitBreakerController {
@@ -19,9 +22,12 @@ public class CircuitBreakerController {
 //    @Retry(name = "default") annotation retries the failing api call.
 //    If name is 'default' it retries for 3 times, custom name can be specified at the application.properties file
 //    fallbackMethod tells which method to be invoked when the request fails
-    @Retry(name = "sample-api", fallbackMethod = "fallbackMethod")
+//    @Retry(name = "sample-api", fallbackMethod = "fallbackMethod")
+
+//    https://resilience4j.readme.io/docs/circuitbreaker
+    @CircuitBreaker(name = "sample-api", fallbackMethod = "fallbackMethod")
     public String sampleApi() {
-        logger.info("Sample Api Called.");
+        logger.info("Sample Api Called. Timestamp: {}", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now()));
         ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8081/this-url-fails", String.class);
         return forEntity.getBody();
     }
